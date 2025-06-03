f096face0a47daf7456019356f2992586a77bd91
wicsion <goldin301@gmail.com>
2025-06-03 15:32:17 +0300

    changes

--- gifts_project/settings.py
+++ gifts_project/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modeltranslation',  # Добавьте эту строку
+
     'rosetta',
     'main.apps.MainConfig',
     'accounts.apps.AccountsConfig',
